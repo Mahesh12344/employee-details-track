4ca2981200d240d7259a75c5d7c223a281d468f0
Mahesh12344 <maheshsharma1712@gmail.com>
2020-12-13 13:38:22 +0530

    removed unwanted code

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,17 +4,4 @@ class Employee < ApplicationRecord
   validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|png)$/, :message => 'file type is not allowed (only jpg|jpeg/png images)'
 	validates_uniqueness_of :email_address
 	validates :phone, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
-
-
-	def self.to_csv
-    attributes = %w{id employee_id}
-
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |car|
-        csv << attributes.map{ |attr| car.send(attr) }
-      end
-    end
-  end
 end
