eab89e525f9a833336613fd8df017e77463618c7
Mahesh12344 <maheshsharma1712@gmail.com>
2020-12-13 15:19:19 +0530

    adavanced search changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,12 @@ class DashboardController < ApplicationController
 	def employee_listings
 		if params[:advanced_search].present?
 			employees = Employee.where("employee_name LIKE ?", "%#{params[:employee_name]}%")
+			total_count = employees.size
 		else
 			employees = Employee.all
+			total_count = employees.size
 		end
+		@total_count = total_count
 		@employees = employees.paginate(:page => params[:page], :per_page => 5)
 	end
 
