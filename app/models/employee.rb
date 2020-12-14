8ca8babe0720d5081e52f39b87cc60cd2eee23fe
Mahesh12344 <maheshsharma1712@gmail.com>
2020-12-14 08:28:42 +0530

    removed extra code

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,5 @@
 class Employee < ApplicationRecord
 	has_attached_file :image, :styles => {:thumb => "200x250>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
   validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|png)$/, :message => 'file type is not allowed (only jpg|jpeg/png images)'
 	validates_uniqueness_of :email_address
 	validates :phone, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
